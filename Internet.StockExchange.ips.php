5bd98e045fe210d52031197a7e02834e4951ad88
Wilkware <heiko.wilknitz@wilkware.de>
2024-11-28 13:10:45 +0100

    v2.1.20180319

--- Internet.StockExchange.ips.php
+++ Internet.StockExchange.ips.php
@@ -1,92 +1,112 @@
-<?
+<?php
 ################################################################################
-# Scriptbezeichnung: Internet.StockExchange.ips.php
-# Version:	2.0.20171205
-# Author:	Heiko Wilknitz (@Pitti)
+# Script:   Online.StockExchange.ips.php
+# Version:  2.1.20180319
+# Author:   Heiko Wilknitz (@Pitti)
+#           Erweiterung von Smudo (Umstellung auf Tagesschau) 
 #
 # Börsenticker:
-# 	JSON - Google	
-# 		https://finance.google.com/finance
-# 		ETR:xxx => Xetra
-# 		FRA:xxx => Frankfurt
-# 		https://finance.google.com/finance?q=ETR:ADN1
-# 		https://finance.google.com/finance?q=FRA:ADN1
-#
+#   Google Search:  https://www.google.de/search?q=<WKN>
+#       WKN = Wertpapierkennnummer
+#           ETR:xxx => Xetra
+#           FRA:xxx => Frankfurt
+#       Beispiel:   https://www.google.com/search?q=ETR:BMW
 #
 # Installation:
-#	- WKN-Array unter Konfiguration mit den entsprechenden Werten befüllen
-#	- Script in der Konsole ausführen
+#   - WKN-Array unter Konfiguration mit den entsprechenden Werten befüllen
+#   - Script in der Konsole ausführen
+#
+# ------------------------------ Changelog -------------------------------------
+#
+#   05.12.2017 - Umstellung auf Curl (v2.0)
+#   19.03.2018 - Umstellung von Google Finance auf Google Search (v2.1)
+#   28.11.2024 - Umstellung auf tagesschau.de (v3.0)
 #
 # ------------------------------ Konfiguration ---------------------------------
 #
-$wkn = array(
-	"FRA:ADN1"		=> "adesso AG"	//google
-);
-#
-# ----------------------------------- ID´s -------------------------------------
-#
+# Wertpapierkennnummer
+$wkn = [
+    // Google Search
+    //  "ETR:ADN1"      => "adesso SE",
+    "FRA:ADN1"      => "adesso SE",
+    //  "FRA:DBK"      => "Deutsche Bank",
+];
 #
 ################################################################################
 
-
+// INSTALLATION
 if ($_IPS['SENDER'] == "Execute") {
-	// ID des ArchiveHandler ermitteln 
-	$instances = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}'); 
-	$id_archive_handler = $instances[0]; 
-	// pro WKN eine Variable
-	foreach($wkn as $ident => $name) {
-		$vid = CreateVariableByName($_IPS['SELF'], $name, 2 /*Float*/);
-		IPS_SetInfo($vid, $ident);
-		IPS_SetIcon($vid, "Graph");
-		AC_SetLoggingStatus($id_archive_handler, $vid, true);
-	}
+    // ID des ArchiveHandler ermitteln 
+    $instances = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}'); 
+    $id_archive_handler = $instances[0]; 
+    // pro WKN eine Variable
+    foreach($wkn as $ident => $name) {
+        $pos = 0;
+        $vid = CreateVariableByName($_IPS['SELF'], $name, 2 /*Float*/, $pos++, 'Graph' , 'Stock.ADN1');
+        IPS_SetInfo($vid, $ident);
+        AC_SetLoggingStatus($id_archive_handler, $vid, true);
+    }
+}
+// TIMEREVENT
+if($_IPS['SENDER'] == "TimerEvent") {
+    // alle untergeordneten Objekt einsammeln
+    $ids = IPS_GetChildrenIDs($_IPS['SELF']);
+    //echo print_r($ids);
+    foreach ($ids as $id) {
+        // for each Wertkennzeichen daten holen
+        $array = IPS_GetObject($id);
+        // Float-Variable?
+        if ($array['ObjectType'] == 2) {
+            $url = 'https://www.google.de/search?q='.$array['ObjectInfo'];
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); 
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); 
+            curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'); 
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+            $page = curl_exec($curl);
+            curl_close($curl);
+            $stock = GetMark($page, '<span style="font-size:157%"><b>*</b>'); 
+            //var_dump($page);
+            $dec = ToFloat($stock);
+            //var_dump($dec);
+            if(!is_float((float)$dec)) {
+                IPS_LogMessage("BOERSE", $dec);
+                return;
+            }
+            if($dec > 0) {
+                SetValue($id, $dec);
+            }
+            else {
+                IPS_LogMessage("BÖRSE", $dec);
+            }
+        }
+    }
 }
 
-
-if($_IPS['SENDER'] == "TimerEvent") {
-	// alle untergeordneten Objekt einsammeln
-	$ids = IPS_GetChildrenIDs($_IPS['SELF']);
-	// echo print_r($ids);
-	foreach ($ids as $id) {
-		// for each Wertkennzeichen daten holen
-		$array = IPS_GetObject($id);
-		// Float-Variable?
-		if ($array['ObjectType'] == 2) {
-			// Google
-			$url = 'https://finance.google.com/finance?q='.$array['ObjectInfo'].'&output=json';
-			$req = file_get_contents($url, false, NULL, 4);	// Lesen der Daten.
-			//var_dump($req);
-			$dec = json_decode($req, true);
-			//var_dump($dec);
-			if($dec[0]["l"] > 0) {
-				SetValue($id, $dec[0]["l"]);
-			}
-		}		
-	}
-}	
-
-# ------------------------------ Funktionen ------------------------------------
-
-function CreateVariableByName($id, $name, $type) 
-{ 
-   $vid = @IPS_GetVariableIDByName($name, $id); 
-   if($vid===false) { 
-      $vid = IPS_CreateVariable($type); 
-      IPS_SetParent($vid, $id); 
-      IPS_SetName($vid, $name); 
-   } 
-   return $vid; 
+// Diese Funktion trennt die relevanten Bereiche aus dem Ausschnitt heraus 
+// $string ist dabei der zu durchsuchende Gesamtstring, 
+// in $Mark sind durch "*" getrennt der Beginn des zu suchenden Strings  
+// und das Ende des zu suchende Abschnittes. 
+// Beispiel für den Text "<div>*</div></li>" 
+function GetMark($string, $mark) { 
+	$find = explode("*",$mark); 
+	$lens  = strlen($find[0]);
+	$lene  = strlen($find[1]); 
+	$start = strpos($string, $find[0]); 
+	$stop  = strpos($string, $find[1], $start+$lens);
+	$inner = substr($string, $start+$lens, $stop-$start-$lens);
+	return $inner; 
 } 
 
-function CreateEventByName($id, $name, $type) 
-{ 
-   $eid = @IPS_GetEventIDByName($name, $id); 
-   if($eid===false) { 
-      $eid = IPS_CreateEvent($type); 
-      IPS_SetParent($eid, $id); 
-      IPS_SetName($eid, $name); 
-   } 
-   return $eid; 
+// Umwandlung einer Zahlenrepräsentations als String in eine echte Gleitkommazahl
+function ToFloat($str) {
+  if(strstr($str, ",")) { 
+    $str = str_replace(".", "", $str); // replace dots (thousand seps) with blancs 
+    $str = str_replace(",", ".", $str); // replace ',' with '.' 
+  } 
+  return floatval($str); // take some last chances with floatval 
 }
 
 ################################################################################
